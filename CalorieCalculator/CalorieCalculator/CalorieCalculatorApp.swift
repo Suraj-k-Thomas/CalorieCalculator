830b8b8c3791241ffc4d615851d3d62ca6280a29
surajkthomas <ksurajt@gmail.com>
2025-05-24 15:41:17 +0530

    setup

---
+++ CalorieCalculator/CalorieCalculator/CalorieCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieCalculatorApp.swift
+//  CalorieCalculator
+//
+//  Created by Suraj  Thomas on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CalorieCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalorieCalculator/CalorieCalculatorTests/CalorieCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieCalculatorTests.swift
+//  CalorieCalculatorTests
+//
+//  Created by Suraj  Thomas on 24/05/25.
+//
+
+import Testing
+@testable import CalorieCalculator
+
+struct CalorieCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
